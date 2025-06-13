c6e943297a128d0e5ef413d06614795435febd8e
RaymondB05 <Rayanabiad@gmail.com>
2025-06-13 02:25:05 +0100

    Initial commit

--- react_template/src/data/portfolioData.js
+++ react_template/src/data/portfolioData.js
@@ -1,4 +1,5 @@
-import portfolioDataRaw from '/data/chats/pbwzp/workspace/portfolio_data.json';
+import portfolioDataRaw from './portfolio_data.json';
+
 
 // Export the portfolio data for use across components
 export const portfolioData = portfolioDataRaw;
